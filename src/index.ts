26faa7351ec43b19f14534637ab06b15683c0fd5
Zakaria ZIANE <zakariaziane@hotmail.com>
2025-01-13 00:03:14 +0100

    Test Services

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
+import "./lib/utils/autoRegister";
 import { Inject } from "./lib/decorators/Inject";
 import { Container } from "./lib/utils/Container";
-import EmployeeService from "./services/EmployeeService";
 import UserService from "./services/UserService";
 
 class Main {
-  constructor(
-    @Inject("UserService") private userService: UserService,
-    @Inject("EmployeeService") private employeeService: EmployeeService
-  ) {}
+  constructor(@Inject("UserService") private userService: UserService) {}
 
   test() {
     this.userService.print();
-    this.employeeService.print();
+    this.userService.printFromEmployeeService();
   }
 }
-
 const main = Container.create(Main);
 main.test();
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,9 +1,16 @@
+import { Inject } from "../lib/decorators/Inject";
 import { Injectable } from "../lib/decorators/Injectable";
+import EmployeeService from "./EmployeeService";
 
 @Injectable("UserService")
 export default class UserService {
-  constructor() {}
+  constructor(
+    @Inject("EmployeeService") private employeeService: EmployeeService
+  ) {}
   print() {
     console.log("I am a method of UserService");
+  }
+  printFromEmployeeService() {
+    this.employeeService.print();
   }
 }
