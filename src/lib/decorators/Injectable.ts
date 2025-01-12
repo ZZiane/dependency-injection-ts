86612c9ab0b6e525fc902e4f1a7fd01f7645ce26
Zakaria ZIANE <zakariaziane@hotmail.com>
2025-01-13 00:00:02 +0100

    feat: Register nested parameters

--- src/lib/decorators/Injectable.ts
+++ src/lib/decorators/Injectable.ts
@@ -1,7 +1,19 @@
+import { classRegistry } from "../utils/ClassRegister";
 import { Container } from "../utils/Container";
+import "reflect-metadata";
 
 export function Injectable(key: string) {
   return function (constructor: Function) {
-    Container.register(key, new (constructor as any)());
+    const paramTypes: any[] =
+      Reflect.getMetadata("design:paramtypes", constructor) || [];
+    let params = paramTypes.map((paramType: any) => {
+      try {
+        return Container.resolve(paramType);
+      } catch (error) {
+        return Container.register(paramType, classRegistry.getClass(paramType));
+      }
+    });
+    const instance = new (constructor as any)(...params);
+    Container.register(key ?? constructor.name, instance);
   };
 }
